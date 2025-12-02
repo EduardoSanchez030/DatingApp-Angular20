d11417c4a5cdb6b2a038d271587f2b3f735f627d
Martin <eduardosanchez030@gmail.com>
2025-12-02 12:56:08 -0600

    End of section 4

---
+++ API/Interfaces/ITokenService.cs
@@ -0,0 +1,8 @@
+using DatingApp.API.Entities;
+
+namespace API.Interfaces;
+
+public interface ITokenService
+{
+    string CreateToken(AppUser user);
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,23 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DatingApp.API.Data.AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors();
+builder.Services.AddScoped<API.Interfaces.ITokenService, API.Services.TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("Cannot get token key - Program.cs");
+        
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {          
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                tokenKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
@@ -16,5 +34,7 @@ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
                 .WithOrigins("https://localhost:4200", "http://localhost:4200"));
 
 app.MapControllers();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
