6e6de9451b19ea5ed235fa436509d19181d70ea8
Martin <eduardosanchez030@gmail.com>
2025-12-05 16:33:02 -0600

    End of section 10

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,4 +1,7 @@
+using System.Security.Claims;
+using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -32,5 +35,35 @@ public class MembersController(IMemberRepository memberRepository) : BaseApiCont
     public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string memberId)
     {
         return Ok(await memberRepository.GetPhotosForMemberAsync(memberId));
+    }
+
+    [Authorize]
+    [HttpPut]
+    public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+    {
+        var memberId = User.GetMemberId();
+        if (memberId == null)
+        {
+            return BadRequest("No Id found in token");
+        }
+
+        var member = await memberRepository.GetMemberForUpdateAsync(memberId);
+
+        if (member == null)
+        {
+            return BadRequest("Could not get member");
+        }
+        
+        member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+        member.Description = memberUpdateDto.Description ??  member.Description;
+        member.City = memberUpdateDto.City ?? member.City;
+        member.Country = memberUpdateDto.Country ?? member.Country;
+
+        member.User.DisplayName =  memberUpdateDto.DisplayName ?? member.User.DisplayName;
+
+        //memberRepository.Update(member);
+        if(await memberRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update member");
     }
 }
